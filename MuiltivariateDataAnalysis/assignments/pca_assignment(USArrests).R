dcf74bab85aa8a782cffe55b893095d82698df76
hyeji choi <hjchoi2323@gmail.com>
2022-03-15 13:55:25 +0900

    pca assignment init

---
+++ MuiltivariateDataAnalysis/assignments/pca_assignment(USArrests).R
@@ -0,0 +1,7 @@
+####################################################
+### 주성분 분석(principal component analysis)
+####################################################
+# 1. 자료 가져오기
+data("USArrests")
+head(USArrests)
+summary(USArrests)
