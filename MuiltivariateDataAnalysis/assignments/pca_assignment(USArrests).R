9a42528aa5ba8de83973eba558920943efd02e2a
hyeji choi <hjchoi2323@gmail.com>
2022-03-15 15:32:17 +0900

    used princomp

--- MuiltivariateDataAnalysis/assignments/pca_assignment(USArrests).R
+++ MuiltivariateDataAnalysis/assignments/pca_assignment(USArrests).R
@@ -5,3 +5,34 @@
 data("USArrests")
 head(USArrests)
 summary(USArrests)
+boxplot(USArrests)
+write.csv(USArrests, file='C:/data/mva/USArrests.csv')
+
+# 2. 주성분 분석 실행
+usarr_pca = princomp(USArrests, cor=T, scores=T)
+names(usarr_pca)
+usarr_pca
+
+# 3. 주성분 분석 결과
+summary(usarr_pca)
+eig_val = usarr_pca$sdev^2
+round(eig_val, 3)
+
+# 5. 스크리그림과 누적분산
+# 스크리그림
+screeplot(usarr_pca, type="lines", pch=19, main="Scree plot")
+# 누적분산 그림 그리기
+usarr_var = usarr_pca$sdev^2
+usarr_var_ratio = usarr_var/sum(usarr_var)
+round(usarr_var_ratio, 3)
+plot(cumsum(usarr_var_ratio), type='b', pch=19, xlab='Component', 
+     ylab='Cumulative Proporton')
+title('Variance Explained')
+
+# 6. 주성분 계수
+round(usarr_pca$loadings[,c(1:2)], 3)
+
+# 7. 주성분 점수 및 행렬도(biplot)
+usarr_pca$scores[c(1:7), c(1:2)]
+biplot(usarr_pca, cex=0.7, col=c("Red", "Blue"))
+title("Biplot")
