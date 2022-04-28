31398248986eab26bdb04e4c5ccd922f8c7ed151
Hyeji Choi <hjchoi2323@gmail.com>
2022-04-28 09:02:30 +0900

    gym_mrm_1

--- regression analysis/gym_mrm.R
+++ regression analysis/gym_mrm.R
@@ -6,36 +6,25 @@
 #########################################
 
 # 자료 파일 읽기
+setwd('/Users/hj1/Desktop/R/reg')
 install.packages("xlsx")
 library(xlsx)
-gym = read.xlsx('/Users/hj1/Desktop/R/reg/p162.csv')
+excel <- "p162.csv"
+gym <- read.xlsx(excel, 1)
+head(gym)
 
-# 자료를 읽어 산점도 그리기
-forbes = read.table("c:/data/reg/forbes.txt", header=T)
-head(forbes, 3)
-attach(forbes)
-Lpress = 100 * log(10*press, base=10)
-Lpress
-plot(temp, Lpress, pch=19)
+# 기술통계량 및 상관계수 보기
+summary(gym[,-1]) # 번호라서 ㄱ첫번째 열 빼준다 
+cor(gym[,-1])
 
-# 회귀모형 적합하기
-forbes.lm = lm(Lpress ~ temp, data=forbes)
-summary(forbes.lm)
+# 회귀모형 적합하기 
+gym.lm = lm(Y ~ X1+X2+X3+X4, data=gym)
+summary(gym.lm)
+library(car)
+avPlots(gym.lm)
 
-# 분산분석표 구하기
-anova(forbes.lm)
+# 분산분석표
+anova(gym.lm)
 
-# 잔차 및 추정값 보기
-names(forbes.lm)
-cbind(forbes, forbes.lm$residuals, forbes.lm$fitted.values)
-
-# 잔차 그림 그리기
-plot(temp, forbes.lm$residuals, pch=19)
-abline(h=0, lty=2)
-
-# 추정값의 신뢰대 그리기
-p.x = data.frame(temp=c(1, 45))
-pc=predict(forbes.lm, int="c", newdata=p.x)
-pred.x = p.x$temp
-plot(temp, Lpress, ylim = range(Lpress, pc))
-matlines(pred.x, pc, lty=c(1,2,2), col="BLUE")
+# 잔차산점도 그리기
+plot()
